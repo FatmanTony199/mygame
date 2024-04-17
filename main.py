cfbb37b03f962c3c94d4ce50e2400bfc0e986fc7
Tony <s210562@email.nlhs.tyc.edu.tw>
2024-04-17 14:28:59 +0800

    Draw a ball on the screen

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import pygame
 import sys
 
+
+def __init__(self, x: int | float, y: int | float, color: list[int] | tuple[int], r: int = 5, direction: int | float = -1):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.r = r
+        if direction == -1:
+            self.d = ri(0, 360)
+        else:
+            self.d = direction
+
+
+def move(self, speed):
+        self.x += sin(self.d / 180 * pi) * speed
+        self.y += cos(self.d / 180 * pi) * speed
+        if self.x > W - self.r or self.x < 0 + self.r:
+            self.d = 360 - self.d
+        if self.y > H - self.r or self.y < 0 + self.r:
+            self.d = 540 - self.d
+            if self.d >= 360:
+                self.d -= 360
 # Initialize Pygame
 pygame.init()
 
@@ -23,6 +44,7 @@ while gameIsRunning:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameIsRunning = False
+        if event.type == 
 
 
     screen.fill((0, 0, 0))
